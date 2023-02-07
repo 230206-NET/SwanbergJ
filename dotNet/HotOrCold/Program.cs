8dcb172211ebbab377da300ed782fbe644fc009a
neiden <pinkgodzela@gmail.com>
2023-02-07 13:14:38 -0800

    Added HotOrCold game

---
+++ dotNet/HotOrCold/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HotOrCold
+{
+
+    public class HOC
+    {
+
+        public static void Main(string[] args)
+        {
+            var rand = new Random();
+            int target = rand.Next(20);
+            int userInput = -1;
+
+            Console.WriteLine("Welcome to Hot or Cold! Try to guess a number 0 - 20!");
+            while (userInput != target)
+            {
+                userInput = Int32.Parse(Console.ReadLine());
+                if (userInput < target)
+                {
+                    Console.WriteLine("Too Cold! Try Again");
+                }
+                if (userInput > target)
+                {
+                    Console.WriteLine("Too Hot! Try Again");
+                }
+            }
+
+            Console.WriteLine("Congratulations! The number was " + target + ". Thanks for playing!");
+
+
+
+        }
+
+    }
+}
