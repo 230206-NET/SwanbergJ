2a7f058845f800d8e179daee0cefba2770966dbb
neiden <pinkgodzela@gmail.com>
2023-02-07 12:47:29 -0800

    Added CoinFlipper

---
+++ dotNet/CoinFlipper/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace CoinFlipper
+{
+
+    public class Flipper
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Coin Flipper starting ...");
+            var rand = new Random();
+            bool heads = true;
+            String side = "Head";
+            int val = rand.Next();
+
+            if (val % 2 > 0)
+            {
+                heads = false;
+                side = "Tails";
+            }
+
+            Console.WriteLine("It was " + side + "!");
+
+
+
+        }
+    }
+
+}
+
